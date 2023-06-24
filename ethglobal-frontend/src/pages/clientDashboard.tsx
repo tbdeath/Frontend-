9eff166c7584f7cb671b167c01d08e901e5410de
Yicheng Zhang <zychelenzhang@gmail.com>
2023-06-24 11:32:45 -0400

    created the route for client and user

---
+++ ethglobal-frontend/src/pages/clientDashboard.tsx
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>This is the client page</h1>
+  }
+  
+  export default Home
---
+++ ethglobal-frontend/src/pages/userDashboard.tsx
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>This is the user page.</h1>
+  }
+  
+  export default Home
